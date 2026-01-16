df662681583afcabc5d076777b6c81c9b663a335
DsML01 <davimelovas61@gmail.com>
2026-01-16 16:15:20 -0300

    Chore: Get tasks implemented

--- app/routes/tasks.tsx
+++ app/routes/tasks.tsx
@@ -1,5 +1,19 @@
 import type { Route } from "./+types/tasks";
 import { Tasks } from "../features/tasks/tasks";
+import { api } from "~/lib/api/client";
+import { useLoaderData } from "react-router";
+import type { Task } from "~/lib/api/types";
+
+export async function loader() {
+  try {
+    const response = await api.get<Task[]>("/tasks/");
+    return {tasks: response.data}
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    throw new Response("Failed to load tasks", { status: 500 });
+  }
+}
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +22,6 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Index() {
-  return <Tasks />;
+export default function Index({loaderData}: Route.ComponentProps) {
+  return <Tasks initialTasks={loaderData.tasks}/>;
 }
